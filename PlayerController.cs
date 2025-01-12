f13232aa16093ad1bf91722b3b1cdb1eeb5499ff
Boxfort <jackwanderson@protonmail.com>
2025-01-12 23:05:41 +0000

    misc stuff

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,9 +3,36 @@ using System;
 
 public partial class PlayerController : CharacterBody2D
 {
-    public const float speed = 100.0f;
+    public const float speed = 50.0f;
     public const float friction = 15.0f;
 
+    Sprite2D playerSprite;
+
+    public override void _Ready()
+    {
+        playerSprite = GetNode<Sprite2D>("PlayerSprite");
+    }
+
+    public const float jiggleSpeed = 20.0f;
+    double deltaCount = 0;
+
+    public override void _Process(double delta)
+    {
+        deltaCount = (deltaCount + (delta * jiggleSpeed)) % 100;
+
+        if (Velocity != Vector2.Zero) 
+        {
+            // a is -1 to 1
+            float a = (float)Mathf.Sin(deltaCount);
+            GD.Print(deltaCount);
+
+            float rot = a * 10f;
+
+            playerSprite.RotationDegrees = rot;
+        } else {
+            playerSprite.RotationDegrees = Mathf.Lerp(playerSprite.RotationDegrees, 0, (float)delta * jiggleSpeed);
+        }
+    }
 
     public override void _PhysicsProcess(double delta)
     {
