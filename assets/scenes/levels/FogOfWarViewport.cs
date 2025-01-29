9061c943bf6601200fd69b2aab06156b66e2ec13
Boxfort <jackwanderson@protonmail.com>
2025-01-29 22:12:16 +0000

    fog of war misc

--- assets/scenes/levels/FogOfWarViewport.cs
+++ assets/scenes/levels/FogOfWarViewport.cs
@@ -6,4 +6,18 @@ public partial class FogOfWarViewport : SubViewport
     // TODO: get tilemap size x tilesize and set the viewport to that size.
     [Export]
     TileMapLayer staticTilemap;
+
+    public override void _Ready()
+    {
+        int maxX = 0;
+        int maxY = 0;
+
+        foreach (Vector2I coord in staticTilemap.GetUsedCells())
+        {
+            maxX = Math.Max(coord.X, maxX);
+            maxY = Math.Max(coord.Y, maxY);
+        }
+
+        Size = new Vector2I(maxX + 1, maxY + 1) * staticTilemap.TileSet.TileSize;
+    }
 }
